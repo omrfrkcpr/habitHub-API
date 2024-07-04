fe9614b0f94a3a45eeafcb6a680138fb1a29afa0
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-04 23:52:53 +0200

    chore: Add user registration and login functionality

---
+++ src/controllers/authController.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const { comparePassword } = require("../helpers/passwordEncrypt");
+
+exports.register = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    user = new User({ firstName, lastName, email, password });
+
+    await user.save();
+
+    const payload = { user: { id: user.id } };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const messages = Object.values(err.errors).map((val) => val.message);
+      return res.status(400).json({ message: messages.join(", ") });
+    }
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const payload = { user: { id: user.id } };
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
