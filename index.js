f90b88bb1eb3e83ac1a17b53ff3a4819f5cff807
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-04 19:42:34 +0200

    chore: Update npm dependencies and middleware in package.json and index.js

--- index.js
+++ index.js
@@ -3,14 +3,32 @@
 const PORT = process.env?.PORT || 8000;
 const HOST = process.env?.HOST || "127.0.0.1";
 
-const express = require("express");
 require("express-async-error");
-const app = express();
-app.use(express.json());
+const express = require("express");
 require("dotenv").config();
 
-// Middlewares
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "PATCH", "HEAD", "DELETE"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
+app.all("/", (req, res) => {
+  res.send("Welcome to HabitHub API");
+});
+
+// Routers
 app.use(require("./src/routers/todoRouter"));
+
+// Middlewares
 app.use(require("./src/middlewares/errorHandler"));
 
 app.listen(PORT, () => console.log(`server runned on http://${HOST}:${PORT}`));
