0235a0e1167b6edaab2716299b6bfe9da6c377d9
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-07-18 01:57:44 +0200

    refactor: Update package dependencies for passport-facebook
    Update the package.json and package-lock.json files to include the latest version of passport-facebook (v3.0.0) as a dependency. This update is necessary to ensure compatibility with the latest changes in the authentication process for third-party apps.

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -33,18 +33,18 @@ router.get(
   })
 );
 
-// Facebook authentication routes
-router.get(
-  "/facebook",
-  passport.authenticate("facebook", { scope: ["email"] })
-);
-router.get(
-  "/facebook/callback",
-  passport.authenticate("facebook", {
-    successRedirect: "http://localhost:3000/auth/success?auth=facebook",
-    failureRedirect: "http://localhost:3000/auth/failure",
-  })
-);
+// // Facebook authentication routes
+// router.get(
+//   "/facebook",
+//   passport.authenticate("facebook", { scope: ["email"] })
+// );
+// router.get(
+//   "/facebook/callback",
+//   passport.authenticate("facebook", {
+//     successRedirect: "http://localhost:3000/auth/success?auth=facebook",
+//     failureRedirect: "http://localhost:3000/auth/failure",
+//   })
+// );
 
 // Google authentication routes
 router.get(
