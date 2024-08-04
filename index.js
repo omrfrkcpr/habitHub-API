3e7fa769466c7515b62b15150382129ab5ec7db5
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-08-04 12:20:39 +0200

    [refactor] Add cors options - allowed client url origin

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     process.env.CLIENT_URL,
+    `${process.env.CLIENT_URL}/`,
     "http://localhost:3000",
     "http://127.0.0.1:3000",
   ],
