de38f0af67a7462732ac8a44478c044b73c5aea1
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-08-04 12:29:46 +0200

    {fix} Update express.urlencoded configuration for improved data handling
    Update the configuration for express.urlencoded in the index.js file to set the "extended" option to true. This change allows the server to handle URL-encoded form data with nested objects and arrays. By enabling this option, we improve the data handling capabilities of the server, ensuring compatibility with various types of form submissions.

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const cors = require("cors");
 const corsOptions = {
   origin: [
     process.env.CLIENT_URL,
-    `${process.env.CLIENT_URL}/`,
     "http://localhost:3000",
     "http://127.0.0.1:3000",
   ],
@@ -68,7 +67,7 @@ app.use(passport.session()); // session data controller
 app.use(express.json({ limit: "10kb" }));
 
 // Accept FormData
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
